cadb295571daebb7945c4c7f6df2f941c6973274
Matthias Gutsch <MGutsch@datacolor.com>
2023-08-03 13:38:01 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { State } from './state/app.state';
+import { Store } from '@ngrx/store';
+import { getShowProductFilter } from './state';
+import { ProductPageActions } from './state/actions';
 
 
 @Component({
@@ -6,6 +11,19 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  displayFilter$: Observable<boolean>;
+
+  constructor(private store: Store<State>) { }
+
+  ngOnInit(): void {
+  this.displayFilter$ = this.store.select(getShowProductFilter);
+
+  }
+
+  checkChangedFilter(): void {
+    this.store.dispatch(ProductPageActions.toggleProductFilter());
+
+  }
 
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SidebarComponent {
+  @Input()  displayFilter: boolean | null;
+  @Output() displayFilterChanged = new EventEmitter<void>();
 
 
+  checkChangedFilter(): void {
+    this.displayFilterChanged.emit();
+    
+  }
 
 
 }
